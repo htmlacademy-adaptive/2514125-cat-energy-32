c6ebd7035c34f51d090399ef3c245176364f1341
Faretos <165820990+Faretos@users.noreply.github.com>
2024-08-26 18:21:17 +0300

    Modul6-task1 (#14)
    * Добавляет наставника
    
    * Добавляет ссылку на наставника
    
    * Исправляет добавление ссылки наставника
    
    * Исправляет добавление ссылки наставника
    
    * Исправляет ссылку наставника
    
    * Тестируем сборку
    
    * Старт разметки
    
    * Сырой вариант разметки главной стр
    
    * Добавление названия для главной стр
    
    * Небольшая коррекция разметки
    
    * Коррекция разметки главной стр
    
    * Разметка главной стр и каталога
    
    * Легкое исправление разметки
    
    * Разметка главной стр и каталога
    
    * Merge branch 'master' into modul2-task1
    
    * Коррекция тега input
    
    * Merge branch 'master' into modul2-task1
    
    * Патч для БЭМ соответствия
    
    * Добавление графики. Начало
    
    * Добавление графики в head
    
    * Добавлеине изображений в стр каталога
    
    *  Добавление логотипа HTML Academy в footer
    
    *  Небольшое исправление
    
    * Небольшое исправление имен классов
    
    * Исправления на стр каталога
    
    * Исправление subscription
    
    * Исправление орфографии
    
    * Небольшое исправление разметки в каталоге
    
    * Коррекция social-list
    
    * Исправление разметки главно
    
    * Небольшой патч
    
    * Merge branch 'master' into modul3-task1
    
    * Изменения класса для соответствия БЭМ
    
    * Добавление Favicon
    
    *  Редакция изображений
    
    * Порядок в местоположениях в картинках
    
    * Добавление Заголовкой у article на стр каталога
    
    * Добавление классов в logo
    
    * Добавление файлов SCSS для Блоков
    
    * Добавление блока mixins
    
    * Исправления после созвона
    
    * Исправление после созвона #2
    
    * Исправление после созвона #3
    
    * Исправление после созвона #4
    
    * Исправление после созвона #5
    
    * Начало базовой стилизации
    
    * Добавление Mixin в шрифты и их использование
    
    * Коррекция файла fonts.scss
    
    * Добавление адаптивной базовой стилизации
    
    * Добавление иконок на index

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,18 @@
 /* в этот файл добавляет скрипты*/
+const mainNav = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+const siteList = document.querySelector('.site-list');
+
+mainNav.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (mainNav.classList.contains('main-nav--closed')) {
+    mainNav.classList.remove('main-nav--closed');
+    mainNav.classList.add('main-nav--opened');
+    siteList.classList.add('site-list--open');
+  } else {
+    mainNav.classList.add('main-nav--closed');
+    mainNav.classList.remove('main-nav--opened');
+    siteList.classList.remove('site-list--open');
+  }
+});
